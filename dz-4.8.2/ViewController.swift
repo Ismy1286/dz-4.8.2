9f6dae01cba0173765bc3dddc604deedf80c7704
Ainur <ismy1286@gmail.com>
2022-01-20 15:19:23 +0600

    InitialCommit

--- dz-4.8.2/ViewController.swift
+++ dz-4.8.2/ViewController.swift
@@ -6,14 +6,70 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+import SnapKit
 
 
+class Cafe: Codable {
+    var cafe: [CafeModel] = []
+    var coment: ComentModel? = nil
 }
 
+class CafeModel: Codable {
+    var title: String? = nil
+    var id: Int? = nil
+    var image: String? = nil
+    var bord: [Int] = []
+}
+
+class ComentModel: Codable {
+    var id: String? = nil
+}
+
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        
+        let json = """
+{
+  "cafe" : [
+    {
+      "title": "test1",
+      "id": 1,
+      "image": "URL",
+      "bord": [1, 2, 3, 4]
+    },
+    {
+      "title": "test1",
+      "id": 1,
+      "image": "URL",
+      "bord": [1, 2, 3, 4]
+    },
+    {
+      "title": "test1",
+      "id": 1,
+      "image": "URL",
+      "bord": [1, 2, 3, 4]
+    },
+    {
+      "title": "test1",
+      "id": 1,
+      "image": "URL",
+      "bord": [1, 2, 3, 4]
+    },
+    {
+      "title": "test1",
+      "id": 1,
+      "image": "URL",
+      "bord": [1, 2, 3, 4]
+    }
+  ],
+  "coment": {
+    "id": "test"
+  }
+}
+"""
+        
+        let model = try! JSONDecoder().decode(Cafe.self, from: Data(json.utf8))
+        dump(model)
+    }
+}
